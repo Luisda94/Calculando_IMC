b52984188627db07882eddddc43e1255af581f2e
Luisda <luisda915@gmail.com>
2024-10-18 19:45:55 -0300

    Se procede a comenzar a hacer un seudo-codigo ( las instrucciones )

---
+++ src/modelo/Persona.java
@@ -0,0 +1,24 @@
+package modelo;
+
+public class Persona {
+/*
+ * EJERCICIO:
+Instrucciones para el desarrollo de la actividad:
+● En esta actividad deberás crear una clase persona con los siguientes atributos nombre,
+edad, peso y altura
+● Los atributos solo deben ser accedidos a través de métodos.
+● Deberás crear un método constructor con todos los atributos como argumento.
+● Deberás crear otra clase con un método main donde creará 5 instancias de clase (5
+personas) y deberá evaluar, para cada una, el IMC y revisar si es mayor o menor de
+edad.
+● Finalmente, deberá imprimir todos sus objetos. */
+	
+	//primero procedo a declarar las variables y crear el molde o el paquete modelo
+	//segundo en el paquete modelo creo la clase persona
+	//tercero procedo a crear los atriubos que me solicitan
+	//cuarto creo el metodo constructor
+	//quinto se crea los getters y los setters
+	//sexto se crea los metodos toString para poder sobrescribir de ser necesasio
+	
+	
+}
